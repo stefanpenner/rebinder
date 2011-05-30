8f732a07479a28227700f9cac3d8f7af08db8089
Burke Libbey <burke@burkelibbey.org>
2011-05-30 16:38:03 -0500

    More scary hacks

--- lib/rebind.rb
+++ lib/rebind.rb
@@ -1,5 +1,40 @@
+# -*- coding: utf-8 -*-
+
 class Method
   def rebind(*args,&block)
     unbind.bind(*args,&block)
   end
+  alias ✈ rebind
 end
+
+class UnboundMethod
+  def to_proc
+    proc { |obj| self.bind(obj).call }
+  end 
+end 
+
+class Module
+
+  class BadIdeaProxy < BasicObject
+
+    def initialize(obj)
+      @obj = obj
+    end
+    
+    def method_missing(s)
+      @obj.instance_method(s)
+    end 
+
+  end 
+
+  def ☃
+    BadIdeaProxy.new(self)
+  end 
+
+end 
+
+# id = Object.☃.object_id
+#  #=> #<UnboundMethod: Object(Kernel)#object_id>
+#  
+# [:omg, :wtf, :lol].map(&id)
+#  #=> [507368, 507528, 507688] 
